11467868b642ce2cf000e7fbce68f4593e3009e1
chh147 <chh147@pitt.edu>
2022-09-22 14:05:35 -0400

    Prep project files

--- capstone-project/urls.py
+++ capstone-project/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from storyer import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', views.index, name='index'),
+]] + static(settings.STATIC_URL, document_root=settings.STATIC_URL)
